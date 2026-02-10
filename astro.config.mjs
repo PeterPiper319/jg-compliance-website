8d15e3aee8fd5e00b23aa79d121e0250b65c75a0
Petrus <hornpetrus@yahoo.com>
2026-02-10 17:30:40 +0200

    Fix: Added site URL to resolve sitemap build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // FIX: This URL is required for the sitemap to build.
-  // Once you connect your custom domain, this will be correct.
-  // If you are just testing, you can use a placeholder like 'https://example.com'
-  // but it's better to put your intended final domain here.
-  site: 'https://jgcompliance.co.za', 
-  
+  site: 'https://jgcompliance.co.za', // <--- THIS LINE FIXES THE BUILD ERROR
   integrations: [tailwind(), sitemap()]
 });
