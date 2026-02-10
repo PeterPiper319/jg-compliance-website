4b048f547089c1003edbe13d4f5df03a346d98a3
Petrus <hornpetrus@yahoo.com>
2026-02-10 17:32:53 +0200

    Emergency: Disable sitemap to unblock deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap"; // <-- COMMENTED OUT
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jgcompliance.co.za', // <--- THIS LINE FIXES THE BUILD ERROR
-  integrations: [tailwind(), sitemap()]
+  site: 'https://jgcompliance.co.za', 
+  integrations: [
+    tailwind() 
+    // sitemap() <-- REMOVED TEMPORARILY
+  ]
 });
